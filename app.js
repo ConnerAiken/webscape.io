7bcb85c654261d2f91ce220e19b5fe8b8ab77fba
Conner Aiken <conner@fittedtech.lcom>
2018-03-03 20:38:22 +0000

    Add example docs for github pages

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-(function() {
+/**
+	App entry point, react renders application using this base component.
+	@name app
+	@type {func}
+ */
 
 ReactDOM.render(
   <h1>Hello, world!</h1>,
   document.getElementById('app');
 );
 
-})();
